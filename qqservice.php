67885e04d0a9caefdfb90d573a858ae0e32207ca
lampkid <chunmansun@foxmail.com>
2014-07-30 23:03:55 +0800

    [ADD]component [QQ online service] added

---
+++ qqservice.php
@@ -0,0 +1,37 @@
+﻿
+<link href="<?php echo get_template_directory_uri(); ?>/qqservice/css/qqservice.css" rel="stylesheet" type="text/css" />
+
+<div class="scrollsidebar" id="hhService">
+	<div class="side_content">
+		<div class="side_list">
+			<div class="side_title">
+				<a title="隐藏" class="close_btn"><span>关闭</span></a></div>
+			<div class="side_center">
+				<div class="qqserver">
+					<p>
+					<a title="点此与我交流" href="http://wpa.qq.com/msgrd?v=3&uin=123456789&site=qq&menu=yes" target="_blank">
+					<img src="http://wpa.qq.com/pa?p=1:123456789:51">在线交流</a></p>
+					<p>
+					<a title="点此与我交流" href="http://wpa.qq.com/msgrd?v=3&uin=123456789&site=qq&menu=yes" target="_blank">
+					<img src="http://wpa.qq.com/pa?p=1:123456789:51">在线交流</a></p>
+					<p>
+					<a title="点此与我交流" href="http://wpa.qq.com/msgrd?v=3&uin=123456789&site=qq&menu=yes" target="_blank">
+					<img src="http://wpa.qq.com/pa?p=1:123456789:51">在线交流</a></p>
+					<p>
+					<a title="点此与我交流" href="http://wpa.qq.com/msgrd?v=3&uin=123456789&site=qq&menu=yes" target="_blank">
+					<img src="http://wpa.qq.com/pa?p=1:123456789:51">在线交流</a></p>
+				</div>
+				<strong>咨询热线：123456</strong><div class="msgserver">
+					<p><a href="#">在线客服</a></p>
+				</div>
+			</div>
+			<div class="side_bottom">
+			</div>
+		</div>
+	</div>
+	<div class="show_btn"><span>在线客服</span></div>
+</div>
+
+
+<script src="<?php echo get_template_directory_uri(); ?>/qqservice/js/jquery.hhService.js" type="text/javascript"></script>
+<script>$(function(){$("#hhService").fix({float:'right',minStatue:true,skin:'green',durationTime:1000})});</script>
