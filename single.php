7fb755096fa5429634d5a37ee233f485aac88a5a
lampkid <chunmansun@foxmail.com>
2014-08-05 20:28:21 +0800

    [MOD]article template modified

--- single.php
+++ single.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 /**
  * The Template for displaying all single posts.
  *
@@ -8,25 +8,112 @@
  */
 
 get_header(); ?>
+<style type="text/css" >
+                  
+                    .panel-box {
+                        border:1px solid #428BCA;
+                        
+                        margin-bottom:20px;
+                    }
+					
+					
+                    
+                    .panel-box .panel-box-heading {
+                        background:#428BCA;
+                        width:100%;
+			font-weight:bold;
+                        color:#ffffff;
+                    }
+                    
+                    .panel-box .heading-icon {
+                        margin-left:10px;
+                        margin-right:5px; 
+                        background:url('');
+                    }
+                    
+                    .panel-box .heading-right {
+                        font-weight:normal;
+                        font-size:12px;
+                        position:relative;
+                        margin-right:5px;
+                        float:right;
+                    }
+                    
+                    .panel-box ul {
+                        list-style:disc;
+                        margin-left:30px;
+                    }
 
-		<div id="primary">
-			<div id="content" role="main">
+                    .panel-box ul li{
+                      border-bottom:1px dashed gray;
+                      padding-right:5px;
+                      margin-top:5px;
+                      color:#13152E;
+                    }
+					
+					.post-title {
+					    margin:20px auto auto auto;
+						text-align:center;
+						font-weight:bold;
+						font-size:1.5em;
+						
+					}
+					
+					.post-title a {
+					    border-bottom:1px dashed gray;
+					}
+					
+					.post-info {
+					    text-align:center;
+					}
+					.post-content {
+					    margin:20px 20px;
+					}
+                    
+		</style>
+		  <div class="container" >
+		      <div class="row">
+			      <div class="span4">
+					<div class="panel-box">
+						  <div class="panel-box-heading"> <span class="heading-icon" ></span>Panel Title <span class="heading-right">更多</span></div>
+						  <ul> 
+							<li>item1</li>
+							<li>item2</li>
+							<li>item3</li>
+						  </ul>
+					</div>
+					<div class="panel-box">
+						  <div class="panel-box-heading"> <span class="heading-icon" ></span>Panel Title <span class="heading-right">更多</span></div>
+						  <ul> 
+							<li>item1</li>
+							<li>item2</li>
+							<li>item3</li>
+						  </ul>
+					</div>
+				  </div>
+				  <div class="span8">
+				    <div class="panel-box">
+					    <?php if ( have_posts() ) : ?>
+						  <div class="panel-box-heading"> <span class="heading-icon" ></span><?php $category = get_the_category(); echo $category[0]->cat_name; ?> <span class="heading-right"></span></div>
+						 
+						 
+						    <?php while ( have_posts() ): the_post(); ?>
+							<div class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
+							<div class="post-info" >发布日期：<?php the_date();?></div>
+							<div class="post-content"><?php the_content();?></div>
+							<?php endwhile; ?>
+						  
+						  <?php else: ?>
+						  <?php 
+						     printf('暂无内容'); 
+						     endif;
+						  ?>
+						  
+					</div>
+				  </div>
+			  </div>
+		  </div>
 
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<nav id="nav-single">
-						<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentyeleven' ); ?></h3>
-						<span class="nav-previous"><?php previous_post_link( '%link', __( '<span class="meta-nav">&larr;</span> Previous', 'twentyeleven' ) ); ?></span>
-						<span class="nav-next"><?php next_post_link( '%link', __( 'Next <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?></span>
-					</nav><!-- #nav-single -->
-
-					<?php get_template_part( 'content-single', get_post_format() ); ?>
-
-					<?php comments_template( '', true ); ?>
-
-				<?php endwhile; // end of the loop. ?>
-
-			</div><!-- #content -->
-		</div><!-- #primary -->
+		 
 
 <?php get_footer(); ?>
